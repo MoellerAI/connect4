5d3c09f7bc35440742616dc23a901f0648c65bb1
Mads Møller <madsherlev@hotmail.com>
2021-08-30 22:05:09 +0200

    builded game evaluation and made game playable.

--- core/board.py
+++ core/board.py
@@ -9,7 +9,7 @@ COLUMNS = 7
 class Board():
 
     def __init__(self):
-        self.board_matrix = [[0, 0, 0, 0, 0, 0, 0],
+        self.board = [[0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0],
@@ -23,13 +23,13 @@ class Board():
         for row in range(ROWS):
             for column in range(COLUMNS):
                 if column != COLUMNS-1:
-                    print('|', self.player_icons[self.board_matrix[row][column]], '', end = '')
+                    print('|', self.player_icons[self.board[row][column]], '', end = '')
                 else:
-                    print('|', self.player_icons[self.board_matrix[row][column]], '|', end = '\n')
+                    print('|', self.player_icons[self.board[row][column]], '|', end = '\n')
             print('-   -   -   -   -   -   -   -')
             
     def reset_game(self):
-        self.board_matrix = [[0, 0, 0, 0, 0, 0, 0],
+        self.board = [[0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0],
@@ -38,15 +38,15 @@ class Board():
     
     def make_move(self, player, column):
         for row in range(ROWS - 1):
-            if self.board_matrix[ROWS-1][column] == 0:
-                self.board_matrix[ROWS-1][column] = player
+            if self.board[ROWS-1][column] == 0:
+                self.board[ROWS-1][column] = player
                 break  
-            elif self.board_matrix[row][column] == 0 and self.board_matrix[row + 1][column] != 0:
-                self.board_matrix[row][column] = player
+            elif self.board[row][column] == 0 and self.board[row + 1][column] != 0:
+                self.board[row][column] = player
                 break
     
     def validate_move(self, column: int) -> bool:
-        if self.board_matrix[0][column] != 0:
+        if self.board[0][column] != 0:
             return False
         else:
             return True
@@ -56,30 +56,52 @@ class Board():
             yield 1
             yield 2
         
-    def game_over(self):
-        pass
+    def game_over(self, player) -> bool:
+        # check for horizontal win
+        for column in range(COLUMNS-3):
+            for row in range(ROWS):
+                if self.board[row][column] == player and self.board[row][column+1] == player and self.board[row][column+2] == player and self.board[row][column+3] == player:
+                    return True
 
+        # check for vertical win
+        for column in range(COLUMNS):
+            for row in range(ROWS-3):
+                if self.board[row][column] == player and self.board[row+1][column] == player and self.board[row+2][column] == player and self.board[row+3][column] == player:
+                    return True
+
+        # check negative slope win
+        for column in range(COLUMNS-3):
+            for row in range(ROWS-3):
+                if self.board[row][column] == player and self.board[row+1][column+1] == player and self.board[row+2][column+2] == player and self.board[row+3][column+3] == player:
+                    return True
+
+        # check positive slope win
+        for column in range(COLUMNS-3):
+            for row in range(3, ROWS):
+                if self.board[row][column] == player and self.board[row-1][column+1] == player and self.board[row-2][column+2] == player and self.board[row-3][column+3] == player:
+                    return True
+        return False
     
     def play(self, start_player = 1):
         self.reset_game()
+        game_over = False
         self.print_board()
         player_turn = self.change_player()
         start_player = next(player_turn)
 
-        for i in range(10):
+        while game_over == False:
             while True:
                 column = input(f'Player {self.player_icons[start_player]} choose a column:')
                 if self.validate_move(column = int(column)-1): break
             self.make_move(player = start_player, column = int(column)-1)
             clear()
+            game_over = self.game_over(player = start_player)
             start_player = next(player_turn)
             self.print_board()
+            
 
 
         
 
-
-
-
-board = Board()
-board.play()
+game = Board()
+game.play()
