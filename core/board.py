15342309c62e4cf1e268e1f62be7376eb0be76aa
Mads Møller <madsherlev@hotmail.com>
2021-08-30 11:08:11 +0200

    created different attributes and it is now possible to make moves in the game.

--- core/board.py
+++ core/board.py
@@ -1,3 +1,5 @@
+from helper import clear
+
 import time
 
 ROWS = 6
@@ -36,19 +38,48 @@ class Board():
     
     def make_move(self, player, column):
         for row in range(ROWS - 1):
-            if self.board_matrix[row][column] == 0 and self.board_matrix[row + 1][column] != 0:
-                self.board_matrix[row][column] = player
-                break
-            elif self.board_matrix[ROWS][column] == 0:
+            if self.board_matrix[ROWS-1][column] == 0:
+                self.board_matrix[ROWS-1][column] = player
+                break  
+            elif self.board_matrix[row][column] == 0 and self.board_matrix[row + 1][column] != 0:
                 self.board_matrix[row][column] = player
                 break
     
+    def validate_move(self, column: int) -> bool:
+        if self.board_matrix[0][column] != 0:
+            return False
+        else:
+            return True
+
+    def change_player(self):
+        while True:
+            yield 1
+            yield 2
+        
+    def game_over(self):
+        pass
+
+    
     def play(self, start_player = 1):
         self.reset_game()
+        self.print_board()
+        player_turn = self.change_player()
+        start_player = next(player_turn)
+
+        for i in range(10):
+            while True:
+                column = input(f'Player {self.player_icons[start_player]} choose a column:')
+                if self.validate_move(column = int(column)-1): break
+            self.make_move(player = start_player, column = int(column)-1)
+            clear()
+            start_player = next(player_turn)
+            self.print_board()
+
+
         
 
 
 
 
 board = Board()
-board.print_board()
+board.play()
